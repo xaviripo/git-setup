aef14977ccd794b18f9a4e2730764d851fb7e737
Xavier Ripoll <xaviripo97@gmail.com>
2021-01-05 08:45:05 +0100

    Add quotation marks

--- git-alias.sh
+++ git-alias.sh
@@ -5,7 +5,7 @@ git config --global alias.co checkout
 git config --global alias.ci commit
 
 # Easy on the eye and well aligned log
-git config --global alias.l -c color.ui=always log --pretty="%C(Yellow)%h %C(reset)%ai %C(Cyan)%<(20)%an %C(reset)%x09%s"
+git config --global alias.l '-c color.ui=always log --pretty="%C(Yellow)%h %C(reset)%ai %C(Cyan)%<(20)%an %C(reset)%x09%s"'
 
 # Full backup of the repo -- make sure to git pull first!
-git config --global alias.backup bundle create backup.bundle --all
+git config --global alias.backup 'bundle create backup.bundle --all'
